3ddf6f27e6e411358cdb4be0a90e75fb94f5156b
BackFlowFishStick <82761004+BackFlowFishStick@users.noreply.github.com>
2024-07-07 23:32:02 +0800

    Create CharacterBase&EnemyBase

---
+++ Source/GASLearningProj/Private/Character/CharacterBase.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/CharacterBase.h"
+
+ACharacterBase::ACharacterBase()
+{
+ 	
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ACharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
---
+++ Source/GASLearningProj/Public/Character/CharacterBase.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "CharacterBase.generated.h"
+
+UCLASS(Abstract)
+class GASLEARNINGPROJ_API ACharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ACharacterBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+
+
+};
